bc4a5690a520b2059564d5f93bf2caeddac3548c
Mateus Santos <mateus.freira@gmail.com>
2018-06-02 16:44:34 -0300

    add AFINN-based sentiment analysis as an option

--- service/sentiment/SentimenTask.js
+++ service/sentiment/SentimenTask.js
@@ -1,21 +1,26 @@
 const _ = require('lodash');
+const applyStrategy = {
+    'single-sync': (messages, method) => Promise.resolve(messages.map(method)),
+    'mult-async': (messages, method) => method(messages)
+};
 class SentimenTask {
-	constructor(config){
-		this.config = config;
-	}
-	
-	getInstance() {
-		if(!this.taskIntance) {
-		    const taskClass = _.get(require(this.config.fileName), this.config.className);
-		    this.taskIntance = new taskClass(this.config.config);				
-		}
-		return this.taskIntance;
-	}
-	
-	apply(messages = []){
-		const taskIntance = this.getInstance();
-		return taskIntance[this.config.methodName](messages);
-	}
+    constructor(config) {
+        this.config = config;
+    }
+
+    getInstance() {
+        if (!this.taskIntance) {
+            const _module = require(this.config.fileName);
+            const taskClass = _.get(_module, this.config.className, _module);
+            this.taskIntance = new taskClass(this.config.config);
+        }
+        return this.taskIntance;
+    }
+
+    apply(messages = []) {
+        const taskIntance = this.getInstance();
+        return applyStrategy[this.config.stratefy || 'mult-async'](messages, taskIntance[this.config.methodName].bind(taskIntance));
+    }
 }
 
 module.exports.SentimenTask = SentimenTask;
