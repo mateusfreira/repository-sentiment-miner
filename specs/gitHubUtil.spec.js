f9f3bd02d1eb810425e534c0b083b32edb5929cd
Mateus Santos <mateus.freira@gmail.com>
2018-06-04 07:47:59 -0300

    fix: gitHubUtil test

--- specs/gitHubUtil.spec.js
+++ specs/gitHubUtil.spec.js
@@ -8,12 +8,12 @@ describe("gitHubUtil", () => {
         })
     });
     it("return the data from a simple resource", () => {
-        return gitHubUtil.getData("https://api.github.com/users/mateusfreira", tokens = []).then((r) => {
+        return gitHubUtil.getData("https://api.github.com/users/mateusfreira").then((r) => {
             r.login.should.be.equal("mateusfreira");
         });
     });
     it("should return dependety data when required", () => {
-        return gitHubUtil.getData("https://api.github.com/users/mateusfreira", null, ["followers_url", "following_url"]).then((r) => {
+        return gitHubUtil.getData("https://api.github.com/users/mateusfreira",  ["followers_url", "following_url"]).then((r) => {
             r.login.should.be.equal("mateusfreira");
             r._following.length.should.be.above(10);
             r._followers.length.should.be.above(10);
