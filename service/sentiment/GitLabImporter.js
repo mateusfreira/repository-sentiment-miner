69d76d19ff411f09ee1a473870a70f50dfcd4422
Mateus Santos <mateus.freira@gmail.com>
2018-06-06 09:31:56 -0300

    fist test passing with GitLab

--- service/sentiment/GitLabImporter.js
+++ service/sentiment/GitLabImporter.js
@@ -2,21 +2,41 @@ const config = require('config');
 const _ = require('lodash');
 const Promise = require('bluebird');
 const async = require('async');
+const request = require('request');
+const tokens = _.get(config, 'gitLab.tokens', []);
+
 class GitLabImporter {
     getProject(url) {
+        const projectUrl = _.slice(url.split('/'), -2).join('/');
+        const requestUrl = `https://gitlab.com/api/v4/projects/${encodeURIComponent(projectUrl)}`;
+        return getGitLabData(requestUrl);
     }
-    getPulls(project) {
-    }
+    getPulls(project) {}
     getCommits() {
 
     }
-    getPullComments(pull) {
-    }
+    getPullComments(pull) {}
     getPullCommits(pull) {
 
     }
-    getPullReviews(pull) {
-    }
+    getPullReviews(pull) {}
 }
 
-module.exports.GitHubImporter = GitHubImporter;
+function getGitLabData(uri, method = `GET`) {
+    const headers = {
+        'User-Agent': 'request-node leasdle research project',
+    };
+    if (_.size(tokens) > 0) {
+        headers["Private-Token"] = tokens[0];
+    }
+    const requestData = {
+        method,
+        uri,
+        headers
+    };
+    return Promise.fromCallback(request.bind(request, requestData))
+        .then(response => response.body)
+        .then(JSON.parse);
+}
+
+module.exports.GitLabImporter = GitLabImporter;
---
+++ specs/GitLabImporter.spec.js
@@ -0,0 +1,47 @@
+const { GitLabImporter } = require('../service/sentiment/GitLabImporter.js');
+
+const should = require("should");
+describe("GitLabImporter", () => {
+    const gitLab = new GitLabImporter();
+    it("should return the project data", () => {//Skiped until we add nocks
+        return gitLab.getProject("https://gitlab.com/mateusfreira/node-stackoverflow-jobs").then((r) => {
+            r.name.should.be.equal("node-stackoverflow-jobs");
+        });
+    });
+
+    it("should get all the PRs from a project", async ()=> {
+        const project = await gitLab.getProject('https://gitlab.com/mateusfreira/node-stackoverflow-jobs');
+        const pulls = await gitLab.getPulls(project);
+        pulls.length.should.be.above(0);
+    });
+
+    it("should get all the commits from a project", async ()=> {
+        const project = await gitLab.getProject('https://gitlab.com/mateusfreira/node-stackoverflow-jobs');
+        const commits = await gitLab.getCommits(project);
+        commits.length.should.be.above(0);
+    });
+
+    it("should get all the comments from a project", async ()=> {
+        const project = await gitLab.getProject('https://gitlab.com/mateusfreira/node-stackoverflow-jobs');
+        const [pull] = await gitLab.getPulls(project);
+        pull.title.should.be.equal("Little error when you were typing your name");
+        const [firstComment] = await gitLab.getPullComments(pull);
+        firstComment.body.should.be.equal("thanks");
+    });
+
+    it("should get all the reviews from a pull", async ()=> {
+        const project = await gitLagitLabroject('https://gitlab.com/mateusfreira/node-stackoverflow-jobs');
+        const [pull] = await gitLab.getPulls(project);
+        pull.title.should.be.equal("Little error when you were typing your name");
+        const [firstComment] = await gitLab.getPullReviews(pull);
+        firstComment.body.should.be.equal("LGTM");
+    });
+
+    it("should get all the commits from a pull", async ()=> {
+        const project = await gitLab.getProject('https://gitlab.com/mateusfreira/node-stackoverflow-jobs');
+        const [pull] = await gitLab.getPulls(project);
+        pull.title.should.be.equal("Little error when you were typing your name");
+        const [firstCommit] = await gitLab.getPullCommits(pull);
+        firstCommit.commit.message.should.be.equal("little error when typing your name");
+    });
+});
